092acc297001247d1706e01804a073a179e6b2e2
Jacob Sheets <paperboycreates@gmail.com>
2019-09-10 12:10:11 -0400

    insertion sort start

--- Insertion_Sort.py
+++ Insertion_Sort.py
@@ -9,3 +9,19 @@
 
 # Description: Insertion Sort Algorthim
 # ==================================================================== #
+
+def insertion_sort_build():
+    sortArray = []
+
+
+def insertion_sort(sortArray):
+    j = 2
+    for j in sortArray:
+        key = sortArray[j]
+        i = j - 1
+        while i > 0 and sortArray[i] > key:
+             sortArray[i] = sortArray[i+1]
+             i = i - 1
+        sortArray[i+1] = key
+
+def main():
